77da9c412feafea56866a6b9cdc2a968f3cb9c96
selanalex <selanalex04@gmail.com>
2026-07-03 10:18:23 +0530

    Fix typo in button function call

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 name= st.text_input("Enter your name")
+
 st.title("take the input name")
-if st.bottom("Submit Name"):
+if st.buttom("Submit Name"):
   st.write(f"print the name : {name}")
